b31101bfb3070c449fcff0d53f54e0f9cdb314df
KilluA <omargarza394@gmail.com>
2024-08-07 13:26:26 -0600

    ChaT3nshi v1.0

--- share.js
+++ share.js
@@ -1,14 +1,32 @@
-const connections = [];
+// const connections = [];
 
-onconnect = (event)=>{
-    const port = event.ports[0];
-    connections.push(port);
+// onconnect = (event)=>{
+//     const port = event.ports[0];
+//     connections.push(port);
     
-    port.onmessage = (e)=> {
-        connections.forEach(conn =>{
-            if(conn !==port){
-                conn.postMessage(e.data);
-            }
-        });
-    };
-};
+//     port.onmessage = (e)=> {
+//         connections.forEach(conn =>{
+//             if(conn !==port){
+//                 conn.postMessage(e.data);
+//             }
+//         });
+//     };
+// };
+
+const express = require('express');
+const http = require('http')
+const socketIo = require('socket.io')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('conection',(socket)=>{
+    console.log('se conecto un usuario');
+    socket.on('chat mensage', (msg) => {
+        io.emit('chat mensage', msg);
+    });
+});
+server.listen(3000, () => {
+    console.log('listen');
+});
